a7e20b1d9ca97db0a664d04329fbf232c39253ce
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-01 13:32:11 +0300

    added lesson02

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
-alert('hello');
-console.log('test');
+let title = 'project1',
+    screens = 'Простые, Сложные, Интерактивные',
+    screenPrice = 1500,
+    rollback = 5,
+    fullPrice = 150000,
+    adaptive = true;
 
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback/100));
