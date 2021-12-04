1c59fc57a2f79b0a4b3ebaf7ae58c5cdf1685bbe
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-04 23:28:08 +0300

    added lesson05

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 'use strict'
-let title = prompt('как называется ваш проект?'),
-    screens = prompt('Какие типы экранов нужно разработать?'),
-    screenPrice = +prompt('Сколько будет стоить данная работа?'),
-    rollback = 5,
-    adaptive = confirm('Нужен ли адаптив на сайте?'),
-    service1 = prompt('Какой дополнительный тип услуги нужен?'),
-    servicePrice1 = +prompt('Сколько это будет стоить?'),
-    service2 = prompt('Какой дополнительный тип услуги нужен?'),
-    servicePrice2 = +prompt('Сколько это будет стоить?')
+let title,
+    screens,
+    screenPrice,
+    rollback,
+    adaptive,
+    allServicePrices, 
+    servicePercentPrice, 
+    fullPrice, 
+    service1, 
+    service2
 
-let allServicePrices, servicePercentPrice, fullPrice
+const isNumber = (num) => {
+  return !isNaN(parseFloat(num) && isFinite(num))
+}
+
+const validatePromptIsNum = (question) => {
+  let result
+  do {
+    result = prompt(question)
+  } while(!isNumber(result))
+
+  return parseFloat(result)
+} 
+
+const asking = () => {
+  title = prompt('как называется ваш проект?')
+  screens = prompt('Какие типы экранов нужно разработать?')
+  screenPrice = validatePromptIsNum('Сколько будет стоить данная работа?')
+  rollback = 5
+  adaptive = confirm('Нужен ли адаптив на сайте?')
+}
 
 const showTypeOf = variable => {
   console.log(variable, typeof variable)
 }
 
-const getAllServicePrices = (...args) => {
-  return args.reduce((sum, elem)=> {
-    return sum + elem
-  })
+const getAllServicePrices = () => {
+  let sum = 0
+
+  for (let i = 0; i < 2; i++){
+    if (i == 0){
+      service1 = prompt('Какой дополнительный тип услуги нужен?')
+    } else if (i == 1) {
+      service2 = prompt('Какой дополнительный тип услуги нужен?')
+    }
+    sum += validatePromptIsNum('сколько это будет стоить?')
+  }
+  return sum
 }
 
 function getFullPrice(price1, price2) {
@@ -45,7 +73,8 @@ const getTitle = (text) => {
   return text.trim()[0].toUpperCase() + text.trim().slice(1).toLowerCase()
 }
 
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2)
+asking()
+allServicePrices = getAllServicePrices()
 fullPrice = getFullPrice(screenPrice, allServicePrices)
 servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
 
