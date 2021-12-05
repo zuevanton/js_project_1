5b0a3ed71bf0bbe38d73c28616dd0be042c16466
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-05 15:37:42 +0300

    added lesson07

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 'use strict'
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    adaptive,
-    allServicePrices, 
-    servicePercentPrice, 
-    fullPrice, 
-    service1, 
-    service2
 
 const isNumber = (num) => {
   return !isNaN(parseFloat(num) && isFinite(num))
@@ -23,65 +13,81 @@ const validatePromptIsNum = (question) => {
   return parseFloat(result)
 } 
 
-const asking = () => {
-  title = prompt('как называется ваш проект?')
-  screens = prompt('Какие типы экранов нужно разработать?')
-  screenPrice = validatePromptIsNum('Сколько будет стоить данная работа?')
-  rollback = 5
-  adaptive = confirm('Нужен ли адаптив на сайте?')
-}
+const appData = {
+  title: '',
+  screens: '',
+  screenPrice: 0,
+  rollback: 10,
+  adaptive: true,
+  allServicePrices: 0, 
+  servicePercentPrice: 0, 
+  fullPrice: 0, 
+  service1: '',
+  service2: '',
 
-const showTypeOf = variable => {
-  console.log(variable, typeof variable)
-}
+  start(){
+    this.asking()
+    this.allServicePrices = this.getAllServicePrices()
+    this.fullPrice = this.getFullPrice()
+    this.servicePercentPrice = this.getServicePercentPrices()
+    this.title = this.getTitle()
+    
+    this.logger()
+  },
 
-const getAllServicePrices = () => {
-  let sum = 0
+  asking(){
+    this.title = prompt('как называется ваш проект?')
+    this.screens = prompt('Какие типы экранов нужно разработать?')
+    this.screenPrice = validatePromptIsNum('Сколько будет стоить данная работа?')
+    this.adaptive = confirm('Нужен ли адаптив на сайте?')
+  },
 
-  for (let i = 0; i < 2; i++){
-    if (i == 0){
-      service1 = prompt('Какой дополнительный тип услуги нужен?')
-    } else if (i == 1) {
-      service2 = prompt('Какой дополнительный тип услуги нужен?')
+  logger(){
+    console.log(this.fullPrice)
+    console.log(this.servicePercentPrice)
+
+    for(let key in this){
+      console.log(key)
     }
-    sum += validatePromptIsNum('сколько это будет стоить?')
+  },
+
+  getAllServicePrices(){
+    let sum = 0
+
+    for (let i = 0; i < 2; i++){
+      if (i == 0){
+        this.service1 = prompt('Какой дополнительный тип услуги нужен?')
+      } else if (i == 1) {
+        this.service2 = prompt('Какой дополнительный тип услуги нужен?')
+      }
+      sum += validatePromptIsNum('сколько это будет стоить?')
+    }
+    return sum
+  },
+
+  getFullPrice() {
+    return +this.screenPrice + this.allServicePrices
+  },
+
+  getServicePercentPrices(){
+    return Math.ceil(this.fullPrice - this.fullPrice * (this.rollback/100))
+  },
+
+  getRollbackMessage(){
+    if (this.fullPrice >= 30000){
+      return 'Даем скидку в 10%'
+    } else if (this.fullPrice >= 15000 && this.fullPrice < 30000){
+      return 'Даем скидку в 5%'
+    } else if (this.fullPrice < 15000 && this.fullPrice >= 0){
+      return 'Скидка не предусмотрена'
+    } else {
+      return 'Что то пошло не так'
+    }
+  },
+
+  getTitle() {
+    return this.title.trim()[0].toUpperCase() + this.title.trim().slice(1).toLowerCase()
   }
-  return sum
 }
 
-function getFullPrice(price1, price2) {
-  return price1 + price2
-}
-
-const getServicePercentPrices = (price, percent) => {
-  return Math.ceil(price - price * (percent/100))
-}
-
-const getRollbackMessage = price => {
-  if (price >= 30000){
-    return 'Даем скидку в 10%'
-  } else if (price >= 15000 && price < 30000){
-    return 'Даем скидку в 5%'
-  } else if (price < 15000 && price >= 0){
-    return 'Скидка не предусмотрена'
-  } else {
-    return 'Что то пошло не так'
-  }
-}
-
-const getTitle = (text) => {
-  return text.trim()[0].toUpperCase() + text.trim().slice(1).toLowerCase()
-}
-
-asking()
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice(screenPrice, allServicePrices)
-servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
-
-showTypeOf(title)
-showTypeOf(fullPrice)
-showTypeOf(adaptive)
-
-console.log(getRollbackMessage(fullPrice))
-console.log(screens.toLowerCase().split(', '))
-console.log(servicePercentPrice)
+appData.start()
